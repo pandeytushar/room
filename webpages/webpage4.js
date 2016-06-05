8ceb5b50ecc335ecb1591fb7fddc7cc3cd571dfa
pandeytushar <thetushar4u@gmail.com>
2016-06-05 12:56:53 +0530

    added

---
+++ webpages/webpage4.js
@@ -0,0 +1,14 @@
+var username =prompt("username");
+if (username === "tusharp"){
+  var pass = prompt("password");
+  if (pass==="niceyear"){
+    alert ("Welcome to automated room Tushar!");
+    //document.write("Which mood are you in?");
+  var tusharp= "Welcome Home!";
+  var home=tusharp.link("webpage6.htm");
+  document.getElementById("home").innerHTML=home;
+  }
+  else {
+  alert("Wrong password!!");
+}
+}
